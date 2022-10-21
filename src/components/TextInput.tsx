c1ef59cc6d4a04118c4b898b2ecbc84178a9435d
HadeSatanist <ap66484@gmail.com>
2022-10-21 15:08:52 -0300

    TextInput adicionado

---
+++ src/components/TextInput.tsx
@@ -0,0 +1,15 @@
+import {clsx} from 'clsx';
+import { ReactNode } from 'react';
+export interface TextInputProps{
+}
+export function TextInput({}: TextInputProps){
+    return (
+        <input 
+        className={clsx(
+        '',
+    )}
+            
+    />
+
+    )
+}
