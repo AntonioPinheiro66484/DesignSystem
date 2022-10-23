bbbf813098c89b29c3fb501125ee10a61207ab34
HadeSatanist <ap66484@gmail.com>
2022-10-23 16:54:26 -0300

    Update component button

--- src/components/Button.stories.tsx
+++ src/components/Button.stories.tsx
@@ -1,5 +1,5 @@
-import { Meta, StoryObj } from '@storybook/react'
 import { Button, ButtonProps } from './Button'
+import { Meta, StoryObj } from '@storybook/react'
 
 export default {
     title: 'Components/Button',
